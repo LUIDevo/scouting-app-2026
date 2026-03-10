97c0af4bf6c6fdeff05099680ba5263ae7c392bc
louis <sanicisspeedandfast@gmail.com>
2026-03-09 21:52:00 -0400

    made it work offline

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'scouting-6632-v1';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './overview.html',
+  './dashboard.html',
+  './manifest.json',
+  './icon.svg',
+  './fonts/IBM_Plex_Mono/IBMPlexMono-Regular.ttf',
+  './fonts/IBM_Plex_Sans/static/IBMPlexSans-Regular.ttf',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
